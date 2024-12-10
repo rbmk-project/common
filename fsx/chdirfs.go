4ab111bc2fff0877748110aee20eef783174c163
Simone Basso <bassosimone@gmail.com>
2024-12-10 14:47:38 +0100

    feat(fsx): introduce file system abstraction (#16)
    This abstraction is based on github.com/spf13/afero but adds specific
    changes that make the code suitable for rbmk.
    
    We will use this new functionality for testing of the rbmk tool and
    possibly also to allow to virtualise the file system, a feature that may
    be useful for embedding rbmk.
    
    This change also required to extend the mocks to mock the new filesystem
    types. The actual shared model between fsx and the mocks package is a
    private model in the internal folder. The fsx package exports all the
    private model definitions to fsx consumers.
    
    While there upgrade dependencies.

---
+++ fsx/chdirfs.go
@@ -0,0 +1,119 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Adapted from: https://github.com/spf13/afero
+//
+
+package fsx
+
+import (
+	"io/fs"
+	"net"
+	"path/filepath"
+	"time"
+)
+
+// NewChdirFS creates a new [FS] where each file name is
+// prefixed with the given directory path.
+func NewChdirFS(dep FS, path string) *ChdirFS {
+	return &ChdirFS{basepath: path, dep: dep}
+}
+
+// ChdirFS is the [FS] type returned by [NewChdirFS].
+//
+// The zero value IS NOT ready to use; construct using [NewChdirFS].
+type ChdirFS struct {
+	// basepath is the base path.
+	basepath string
+
+	// dep is the dependency [FS].
+	dep FS
+}
+
+// Ensure [basePathFS] implements [FS].
+var _ FS = &ChdirFS{}
+
+// realPath returns the real path of a given file name or an error.
+func (rfs *ChdirFS) realPath(name string) string {
+	return filepath.Join(rfs.basepath, name)
+}
+
+// Chmod implements [FS].
+func (rfs *ChdirFS) Chmod(name string, mode fs.FileMode) error {
+	return rfs.dep.Chmod(rfs.realPath(name), mode)
+}
+
+// Chown implements [FS].
+func (rfs *ChdirFS) Chown(name string, uid, gid int) error {
+	return rfs.dep.Chown(rfs.realPath(name), uid, gid)
+}
+
+// Chtimes implements [FS].
+func (rfs *ChdirFS) Chtimes(name string, atime, mtime time.Time) error {
+	return rfs.dep.Chtimes(rfs.realPath(name), atime, mtime)
+}
+
+// Create implements [FS].
+func (rfs *ChdirFS) Create(name string) (File, error) {
+	return rfs.dep.Create(rfs.realPath(name))
+}
+
+// DialUnix implements [FS].
+func (rfs *ChdirFS) DialUnix(name string) (net.Conn, error) {
+	return rfs.dep.DialUnix(rfs.realPath(name))
+}
+
+// ListenUnix implements [FS].
+func (rfs *ChdirFS) ListenUnix(name string) (net.Listener, error) {
+	return rfs.dep.ListenUnix(rfs.realPath(name))
+}
+
+// Lstat implements [FS].
+func (rfs *ChdirFS) Lstat(name string) (fs.FileInfo, error) {
+	return rfs.dep.Lstat(rfs.realPath(name))
+}
+
+// Mkdir implements [FS].
+func (rfs *ChdirFS) Mkdir(name string, mode fs.FileMode) error {
+	return rfs.dep.Mkdir(rfs.realPath(name), mode)
+}
+
+// MkdirAll implements [FS].
+func (rfs *ChdirFS) MkdirAll(name string, mode fs.FileMode) error {
+	return rfs.dep.MkdirAll(rfs.realPath(name), mode)
+}
+
+// Open implements [FS].
+func (rfs *ChdirFS) Open(name string) (File, error) {
+	return rfs.dep.Open(rfs.realPath(name))
+}
+
+// OpenFile implements [FS].
+func (rfs *ChdirFS) OpenFile(name string, flag int, mode fs.FileMode) (File, error) {
+	return rfs.dep.OpenFile(rfs.realPath(name), flag, mode)
+}
+
+// ReadDir implements [FS].
+func (rfs *ChdirFS) ReadDir(name string) ([]fs.DirEntry, error) {
+	return rfs.dep.ReadDir(rfs.realPath(name))
+}
+
+// Remove implements [FS].
+func (rfs *ChdirFS) Remove(name string) error {
+	return rfs.dep.Remove(rfs.realPath(name))
+}
+
+// RemoveAll implements [FS].
+func (rfs *ChdirFS) RemoveAll(name string) error {
+	return rfs.dep.RemoveAll(rfs.realPath(name))
+}
+
+// Rename implements [FS].
+func (rfs *ChdirFS) Rename(oldname, newname string) error {
+	return rfs.dep.Rename(rfs.realPath(oldname), rfs.realPath(newname))
+}
+
+// Stat implements [FS].
+func (rfs *ChdirFS) Stat(name string) (fs.FileInfo, error) {
+	return rfs.dep.Stat(rfs.realPath(name))
+}
---
+++ fsx/fsx.go
@@ -0,0 +1,49 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Adapted from: https://github.com/spf13/afero
+//
+
+/*
+Package fsx allows abstracting the file system.
+
+This package is derived from [afero].
+
+In addition to [afero], we also implement support for dialing
+and listening unix domain sockets, and for Lstat.
+
+[afero]: https://github.com/spf13/afero
+*/
+package fsx
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+
+	"github.com/rbmk-project/common/internal/fsmodel"
+)
+
+// Forward file system constants.
+const (
+	O_CREATE = fsmodel.O_CREATE
+	O_RDONLY = fsmodel.O_RDONLY
+	O_RDWR   = fsmodel.O_RDWR
+	O_TRUNC  = fsmodel.O_TRUNC
+	O_WRONLY = fsmodel.O_WRONLY
+)
+
+// IsNotExist combines the [os.ErrNotExist] check with
+// checking for the [fs.ErrNotExist] error.
+func IsNotExist(err error) bool {
+	return errors.Is(err, fs.ErrNotExist) || os.IsNotExist(err)
+}
+
+// File is an alias for [fsmodel.File].
+type File = fsmodel.File
+
+// Ensure [*os.File] implements [File].
+var _ File = &os.File{}
+
+// FS is an alias for [fsmodel.FS].
+type FS = fsmodel.FS
